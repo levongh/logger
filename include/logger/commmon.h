676db874b9fd70599f1a775a360450d7aeffc91b
Levon Ghukasyan <levon_ghukasyan@live.com>
2020-04-05 19:58:49 +0400

    added make_unique for compilers wich does not have C++14 support

--- include/logger/commmon.h
+++ include/logger/commmon.h
@@ -1,5 +1,11 @@
 #pragma once
 
+/**
+ * @author Levon Ghukasyan
+ * @file common.h
+ * @brief this file contains common interface
+ */
+
 #include <memory>
 #include <string>
 #include <initializer_list>
@@ -17,3 +23,34 @@
 #else
     #define LOG_API
 #endif
+
+namespace details {
+
+/**
+ *
+ * _______________________________
+ * |   Meaning of __cplusplus    |
+ * -------------------------------
+ * | __cplusplus |	C++ Standard |
+ * -------------------------------
+ * |    199711L  |  	C++98    |
+ * |    201103L  |  	C++11    |
+ * |    201402L  |  	C++14    |
+ * |    201703L  |  	C++17    |
+ * -------------------------------
+ *
+ * detailed information you can find by follogin link: https://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique
+ **/
+
+#if __cplusplus >= 201402L
+    using std::make_unique;
+#else
+    template <typename T, typename... Args>
+    std::unique_ptr<T> make_unique(Args&&... args)
+    {
+        static_assert(!std::is_array<T>::value, "array is not supported");
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+#endif
+
+} //!namespace details
